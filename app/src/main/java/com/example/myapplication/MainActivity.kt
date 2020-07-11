80d864445b0b0d956c9d6ec5bf0cd0bbe81e6586
_vishal_jha <vishal.jha@indwealth.in>
2020-07-11 11:52:34 +0530

    Image view and material theme using compose

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,10 +3,22 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.recyclerview.widget.RecyclerView
+import androidx.ui.core.ContentScale
+import androidx.ui.core.Modifier
+import androidx.ui.core.clip
 import androidx.ui.core.setContent
+import androidx.ui.foundation.Image
 import androidx.ui.foundation.Text
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.layout.*
+import androidx.ui.material.MaterialTheme
+import androidx.ui.res.imageResource
+import androidx.ui.text.style.TextOverflow
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 import com.example.myapplication.ui.MyApplicationTheme
+import com.example.myapplication.ui.typography
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +33,33 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    val image = imageResource(R.drawable.header)
+    MaterialTheme {
+        Column(modifier = Modifier.padding(10.dp)) {
+            val imageModifier = Modifier
+                    .preferredHeightIn(maxHeight = 180.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(4.dp))
+            Image(image, modifier = imageModifier,
+                    contentScale = ContentScale.Crop)
+
+            Spacer(Modifier.preferredHeight(16.dp))
+
+            Text(
+                    "A day wandering through the sandhills " +
+                            "in Shark Fin Cove, and a few of the " +
+                            "sights I saw",
+                    style = typography.h6,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis)
+            Text("Davenport, California",
+                    style = typography.body2)
+            Text("December 2018",
+                    style = typography.body2)
+        }
+    }
+
+
 }
 
 @Preview(showBackground = true)
